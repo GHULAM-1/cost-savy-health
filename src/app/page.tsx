84c446bb622709c75d475015fb5492f7a51c6fb6
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-03-27 23:40:20 +0500

    branch: init branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return <div className="flex flex-col items-center justify-center h-screen">
 
-<div className="text-2xl font-bold">Hi <span className="text-blue-500">Maureen</span> , this is the production link of the website  </div>
+<div className="text-2xl font-bold">Hi <span className="text-blue-500">Maureen</span> , this is the stagging link of the website  </div>
   </div>
 }
