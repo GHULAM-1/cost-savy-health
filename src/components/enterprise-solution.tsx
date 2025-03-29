04fbd1dd86616c9bc684998e83eee15c58e44fc7
IbrahimChaudhary <ibrahimchaudhary421@gmail.com>
2025-03-30 03:33:27 +0500

    check and complete landing page static

--- src/components/enterprise-solution.tsx
+++ src/components/enterprise-solution.tsx
@@ -1,44 +1,11 @@
 import React from "react";
-import { PlusSquare, ShieldPlus, Building2, Asterisk } from "lucide-react";
+import { enterpriseSolutionsData } from "@/data/landing-page/enterprise-solutions-data";
 
 const EnterpriseSolutions = () => {
-  const solutions = [
-    {
-      icon: <PlusSquare className="text-[#03363D]" size={24} color="#1D5E5A" />,
-      title: "For providers.",
-      description:
-        "Harness price transparency as a competitive edge without staffing up or spreading thin.",
-      link: "#",
-    },
-    {
-      icon: (
-        <ShieldPlus className=" text-[#03363D]" size={24} color="#1D5E5A" />
-      ),
-      title: "For payers.",
-      description:
-        "Stay ahead of legislation and requirements while driving value-based care decisions.",
-      link: "#",
-    },
-    {
-      icon: <Building2 className=" text-[#03363D]" size={24} color="#1D5E5A" />,
-      title: "For employers.",
-      description:
-        "Empower yourself to shop smart for healthcare plans with our transparency-enabled products.",
-      link: "#",
-    },
-    {
-      icon: <Asterisk className=" text-[#03363D]" size={24} color="#1D5E5A" />,
-      title: "For life sciences.",
-      description:
-        "Overhaul your commercial strategy with data perfect for the busiest CEO or the most meticulous data scientist.",
-      link: "#",
-    },
-  ];
-
   return (
     <section className="max-w-[1800px] mx-auto px-12 py-16 md:py-2 mb-32">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
-        {solutions.map((solution, index) => (
+        {enterpriseSolutionsData.map((solution, index) => (
           <div key={index} className="group">
             <div className="bg-white p-5 rounded-sm border-2 border-gray-200 shadow-sm mb-4 inline-block">
               {solution.icon}
@@ -51,7 +18,7 @@ const EnterpriseSolutions = () => {
             </p>
             <a
               href={solution.link}
-              className="inline-block text-[#03363D] font-medium border-b-1 border-[#03363D]  hover:text-[#1D5E5A] hover:border-[#1D5E5A] transition-colors duration-300"
+              className="inline-block text-[#03363D] font-medium border-b-1 border-[#03363D] hover:text-[#1D5E5A] hover:border-[#1D5E5A] transition-colors duration-300"
             >
               Learn More
             </a>
